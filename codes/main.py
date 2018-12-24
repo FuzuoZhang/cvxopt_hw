889d094b6060e45e21189c8fc87197e11bec71f0
Proprince <zrp1017@163.com>
2018-12-24 14:24:30 +0800

    数值实验部分

--- codes/main.py
+++ codes/main.py
@@ -2,27 +2,22 @@ import cvxpy as cp
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Problem data.
-TOL = 1e-3
-m1 = 3
-m2 = 3
-n = 2
+
+# Generate data
 np.random.seed(1)
 length = 0.3
 A = np.vstack([np.array([0, 0]) + length * (-0.5 + np.random.random((10, 2))),
                np.array([1, 1]) + length * (-0.5 + np.random.random((10, 2)))])
 B = np.vstack([np.array([1, 0]) + length * (-0.5 + np.random.random((10, 2))),
                np.array([0, 1]) + length * (-0.5 + np.random.random((10, 2)))])
-# A = np.vstack([A, np.array([1.2, -0.3])])
-# B = np.vstack([B, np.array([-0.3, -0.3])])
+A = np.vstack([A, np.array([1.2, -0.3])])
+B = np.vstack([B, np.array([-0.3, -0.3])])
 xmin, xmax = -0.4, 1.4
 ymin, ymax = -0.4, 1.25
-# A = np.vstack([np.array([-1, -1]) + length * (-0.5 + np.random.random((10, 2))),
-#                np.array([ 1,  1]) + length * (-0.5 + np.random.random((10, 2)))])
-# B = np.vstack([np.array([ 1, -1]) + length * (-0.5 + np.random.random((10, 2))),
-#                np.array([-1,  1]) + length * (-0.5 + np.random.random((10, 2)))])
-# xmin, xmax = -1.5, 1.5
-# ymin, ymax = -1.5, 1.5
+
+# Parameter Setup
+TOL = 1e-3
+n = 2
 m1 = A.shape[0]
 m2 = B.shape[0]
 H = np.hstack([A, np.ones((m1, 1))])
@@ -32,6 +27,7 @@ D2_SR = cp.Parameter((m2, m2))
 c1 = cp.Parameter(nonneg=True)
 c2 = cp.Parameter(nonneg=True)
 
+# Regularizor Coeff
 c1.value = 0.001
 c2.value = 0.001
 
@@ -63,11 +59,11 @@ prob2 = cp.Problem(objective2, constraints2)
 # Obtain the initial solution
 init_prob1.solve()
 z1_last = z1.value
-print(z1.value)
+print('Init solution z1:', z1_last)
 
 init_prob2.solve()
 z2_last = z2.value
-print(z2.value)
+print('Init solution z2:', z2_last)
 
 # Plot points and planes
 w1 = z1_last / np.linalg.norm(z1_last)
@@ -85,7 +81,6 @@ plt.plot(plane1[:, 0], plane1[:, 1], color='r', label='Plane 1')
 plt.plot(plane2[:, 0], plane2[:, 1], color='b', label='Plane 2')
 plt.title('TWSVM')
 plt.legend()
-# plt.show(); exit(0)
 
 # Iterative procedure
 while True:
@@ -106,8 +101,8 @@ while True:
     else:
         z2_last = z2_cur.copy()
 
-print(z1_cur)
-print(z2_cur)
+print('Final solution z1:', z1_cur)
+print('Final solution z2:', z2_cur)
 
 z1_cur /= np.linalg.norm(z1_cur)
 z2_cur /= np.linalg.norm(z2_cur)
@@ -117,7 +112,7 @@ plane1 = np.array([[x, y] for x in np.arange(xmin, xmax, 0.005)
 plane2 = np.array([[x, y] for x in np.arange(xmin, xmax, 0.005)
                           for y in np.arange(ymin, ymax, 0.005)
                           if abs(z2_cur.dot(np.array([x, y, 1]))) < 0.001])
-# print(plane2.shape);exit(0)
+
 plt.subplot(122)
 plt.scatter(A[:, 0], A[:, 1], color='r', label='Class 1')
 plt.scatter(B[:, 0], B[:, 1], color='b', label='Class 2')
@@ -126,10 +121,4 @@ plt.plot(plane2[:, 0], plane2[:, 1], color='b', label='Plane 2')
 plt.title('L1-TWSVM')
 plt.legend()
 plt.show()
-
-# # The optimal Lagrange multiplier for a constraint is stored in
-# # `constraint.dual_value`
-# print(len(constraints))
-# print(constraints[0].dual_value)
-# print(constraints[1].dual_value)
 
